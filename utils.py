f716d1b71f9c50f3876d798acb62454d53bd65da
Nikhil <nikhilbalamurugan@gmail.com>
2025-09-04 19:15:16 +0530

    changes utils

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ def draw_label(frame, text, x, y, w, h, color):
     Draw bounding box and label on the frame.
     """
     cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
-    cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
+    label = f"{text}"
+    cv2.putText(frame, label, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX,
                 0.6, color, 2, cv2.LINE_AA)
     return frame
