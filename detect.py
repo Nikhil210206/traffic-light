3b5fa24a108e48949f42a984338d55358a73243c
Nikhil <nikhilbalamurugan@gmail.com>
2025-09-04 19:14:31 +0530

    update detect.py

--- detect.py
+++ detect.py
@@ -3,10 +3,11 @@ import numpy as np
 from utils import draw_label
 
 # Define HSV ranges for Red, Yellow, Green
+# Note: Red has two ranges to wrap around the hue spectrum
 COLOR_RANGES = {
-    "Red":    [(0, 100, 100), (10, 255, 255)],
-    "Yellow": [(15, 100, 100), (35, 255, 255)],
-    "Green":  [(40, 100, 100), (90, 255, 255)]
+    "Red":    [((0, 120, 70), (10, 255, 255)), ((170, 120, 70), (180, 255, 255))],
+    "Yellow": [((20, 100, 100), (30, 255, 255))],
+    "Green":  [((40, 70, 70), (90, 255, 255))]
 }
 
 COLOR_BGR = {
@@ -22,12 +23,16 @@ def detect_traffic_lights(frame):
     """
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    for color_name, (lower, upper) in COLOR_RANGES.items():
-        lower = np.array(lower)
-        upper = np.array(upper)
+    for color_name, ranges in COLOR_RANGES.items():
+        mask = np.zeros(frame.shape[:2], dtype="uint8")
+        for (lower, upper) in ranges:
+            lower = np.array(lower, dtype="uint8")
+            upper = np.array(upper, dtype="uint8")
+            mask = cv2.bitwise_or(mask, cv2.inRange(hsv, lower, upper))
 
-        mask = cv2.inRange(hsv, lower, upper)
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((5, 5), np.uint8))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, np.ones((10, 10), np.uint8))
+
 
         contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -35,6 +40,9 @@ def detect_traffic_lights(frame):
             area = cv2.contourArea(cnt)
             if area > 200:  # Filter small noise
                 x, y, w, h = cv2.boundingRect(cnt)
-                draw_label(frame, color_name, x, y, w, h, COLOR_BGR[color_name])
+                aspect_ratio = w / float(h)
+                # Add a simple shape constraint, traffic lights are often close to circular or square
+                if 0.8 <= aspect_ratio <= 1.2:
+                    draw_label(frame, color_name, x, y, w, h, COLOR_BGR[color_name])
 
     return frame
